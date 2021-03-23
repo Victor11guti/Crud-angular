30817d0f0a77c23cc860f780eafe08bd541d8a67
victorguti <victor.guiterrez@correounivalle.edu.co>
2021-03-23 16:08:14 -0500

    Select

--- cliente/src/app/filter.pipe.ts
+++ cliente/src/app/filter.pipe.ts
@@ -8,7 +8,7 @@ export class FilterPipe implements PipeTransform {
   transform(value :any ,arg:any): any{
    const resultadoPaciente =[];
    for(const paciente of value){
-    if(paciente.nombres.toLowerCase().indexOf(arg.toLowerCase())> -1 ){
+    if(paciente.nombres.toLowerCase().indexOf(arg.toLowerCase()) >-1 ){
       console.log('si');
       resultadoPaciente.push(paciente)
      
